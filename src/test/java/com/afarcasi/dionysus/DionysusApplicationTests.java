b49cbcbe7c55910af78b92f5b02b00aa9f25a544
Farcasi Alexandru-Vicentiu <farcasialexandrububu@gmail.com>
2026-01-11 21:23:31 +0200

    Init commit

---
+++ src/test/java/com/afarcasi/dionysus/DionysusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afarcasi.dionysus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionysusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
